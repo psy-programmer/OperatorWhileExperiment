d77013b8d64bac116e886c76c059e9ee21600775
psy-programmer <psy-programmer@outlook.com>
2020-09-26 23:49:57 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+//code by Zoia Klocheva for https://zen.yandex.ru/media/programming_for_beginners/
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int value = 10;
+
+        //    while (value > 0) {
+
+        //      value = value - 1;
+
+        //      System.out.println("Value = " + value);
+        //  }
+
+
+        do {
+            value = value - 1;
+
+            System.out.println("Value = " + value);
+        }
+
+        while (value > 15);
+
+    }
+}
